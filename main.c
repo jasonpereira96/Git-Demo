f331716f57aca74b9ac01808b102acef827da3a7
jasonpereira96 <jasonpereira074@gmail.com>
2022-10-14 12:43:32 -0500

    feature 2

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 int main() {
     int a = 0;
     int b = 0;
+    int b2 = 0;
     int c = 0;
-    int d = 0;
-    return 0;
+    int f = 0;
+    // asda
+    // asd 
+    // asdasd
+    int result = a * b;
+    return result;
+}
+
+int fn3() {
+    // Peter is a cunt
+}
+
+int fn4() {
+    // wala laba dub dub
 }
