5a0a7f7ae852090423f64f381177e3ad38cf50ff
jasonpereira96 <jasonpereira074@gmail.com>
2022-10-14 12:38:15 -0500

    finished feature 1

--- main.c
+++ main.c
@@ -2,6 +2,18 @@ int main() {
     int a = 0;
     int b = 0;
     int c = 0;
-    int d = 0;
-    return 0;
+    int e = 0;
+    // asda
+    // asd 
+    // asdasd
+    int result = a + b;
+    return result;
+}
+
+int fn1() {
+    // i want to fuck
+}
+
+int fn2() {
+    // hula hula hu hu hu
 }
